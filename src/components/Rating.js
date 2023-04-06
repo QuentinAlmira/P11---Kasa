51f60ba2dab93b0bcca82f98b731ca4a2da18b89
Quentin Almira <quentin.almira@gmail.com>
2023-04-06 15:31:29 +0200

    rating update

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,13 +3,15 @@ import GreyStar from "../assets/img/grey_star.png";
 import RedStar from "../assets/img/red_star.png";
 
 const Rating = ({ props }) => {
-  console.log(props);
   const starsArray = [1, 2, 3, 4, 5];
+
+  console.log({ props });
+  starsArray.map((star) => console.log(star));
 
   return (
     <div className="accomodation_infos_host_rating_stars">
       {starsArray.map((star) =>
-        props >= star ? (
+        star <= props ? (
           <img key={star} src={RedStar} alt="Accomodation rating" />
         ) : (
           <img key={star} src={GreyStar} alt="Accomodation rating" />
